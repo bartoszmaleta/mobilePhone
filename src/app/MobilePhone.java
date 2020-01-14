67daa855e230e7a7cdb9b8c3e0ff0edb80676ad3
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-01-14 14:37:03 +0100

    Add createContact()

--- src/app/MobilePhone.java
+++ src/app/MobilePhone.java
@@ -3,7 +3,15 @@ package app;
 import java.util.ArrayList;
 
 public class MobilePhone {
-    private ArrayList<Contact> contacts;
+    private ArrayList<Contact> myContacts;
 
+    private String myNumber;
+
+    public MobilePhone(String myNumber) {
+        this.myNumber = myNumber;
+        this.myContacts = new ArrayList<Contact>();
+    }
+
+    public boolean 
     
 }
