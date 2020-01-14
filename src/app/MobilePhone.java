432b9519e2bedb247dab8d80698d7a72946f9c2d
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-01-14 14:42:16 +0100

    Add addNewContact(), findContact()

--- src/app/MobilePhone.java
+++ src/app/MobilePhone.java
@@ -12,6 +12,27 @@ public class MobilePhone {
         this.myContacts = new ArrayList<Contact>();
     }
 
-    public boolean 
+    public boolean addNewContact(Contact contact) {
+        if (findContact(contact.getName()) >= 0) {
+            System.out.println("Contact is already on file");
+            return false;
+        }
+        myContacts.add(contact);
+        return true;
+    }
     
+    private int findContact(Contact contact) {
+        return this.myContacts.indexOf(contact); // 0 >= if exists, less if it isnt
+    }
+
+    private int findContact(String contactName) {
+        for (int i = 0; i < this.myContacts.size(); i++) {
+            Contact contact = this.myContacts.get(i);
+            if (contact.getName().equals(contactName)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
 }
